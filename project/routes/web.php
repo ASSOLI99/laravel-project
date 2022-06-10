3a4b8be86cf008b926f0a1db1c3bd3bbf2a113da
Bahaazyoud <bahaazyoud2000@gmail.com>
2022-06-10 10:13:02 +0300

    upload

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Admin page route
+Route::get('/home', function () {
+    return view('Admin.index');
+});
