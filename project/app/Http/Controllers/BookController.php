9e3138fa25d60f9f48cbfbdb00ee986b3e6c7a4d
AimanAJ <100359631+AimanAJ@users.noreply.github.com>
2022-06-14 02:34:27 +0300

    main

--- project/app/Http/Controllers/BookController.php
+++ project/app/Http/Controllers/BookController.php
@@ -19,7 +19,6 @@ class BookController extends Controller
     public function show(Request $req)
     {
         if (isset($req->cat_id)) {
-            //$book = Book::find($req->id);
             $book = Book::where('catigory_id', $req->cat_id)->get();
         } else {
             $book = Book::all();
@@ -49,7 +48,7 @@ class BookController extends Controller
     {
         $post = Book::find($id);
         
-        $post->state = 3;
+        $post->state = 0;
         $post->update();
 
         return  redirect('/order/'.session('id'))->with('flash_message', 'data Updated!');
